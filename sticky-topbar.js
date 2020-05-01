e4940947a6001dabc9661173d03e9f14fd22eb8e
zhangxiang <1203901485@qq.com>
2020-05-01 17:30:56 +0800

    对init-swiper.js、smoothly-navigation.js和sticky-topbar三部分使用MVC的模式结构化，此前的模块化只是将js里的代码进行了分类！

--- sticky-topbar.js
+++ sticky-topbar.js
@@ -1,10 +1,26 @@
 !function () {
-    window.addEventListener('scroll', function (x) {
-        // console.log(window.scrollY) 记录y轴上滚动的距离
-        if (window.scrollY > 0) {
-            topNavBar.classList.add("sticky")
-        } else {
-            topNavBar.classList.remove("sticky")
+    var view = document.querySelector("#topNavBar")
+    var controller = {
+        view: null,
+        init: function(){
+            this.view = view
+            this.bindEvents() // this.bindEvents.call(this)
+        },
+        bindEvents: function(){
+            window.addEventListener('scroll', ()=>{  // 箭头函数没有this
+                if (window.scrollY > 0) {
+                    this.active()
+                } else {
+                    this.deactive()
+                }
+            })
+        },
+        active: function(){
+            this.view.classList.add("sticky")
+        },
+        deactive: function(){
+            this.view.classList.remove("sticky")
         }
-    })
+    }
+    controller.init()  // controller.init.call(controller, view)
 }.call()
